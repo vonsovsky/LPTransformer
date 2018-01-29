ea9214f35e23a95c883492df80bcbbf59b2dd492
Harry <jakub.vonsovsky@gmail.com>
2018-01-29 15:15:40 +0100

    Better management of model examples in Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,42 +10,52 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        saveSimpleTriangle();
+        transformData(getSimpleTriangle(), "triangle.lp");
+        transformData(getSimpleRectangle(), "rectangle.lp");
+        //transformData(getRandomDistantPoints(), "random.lp");
     }
 
-    private static void saveSimpleTriangle() {
-        Writer writer = new TransformerWriter("triangle.lp");
+    private static void transformData(List<Position> items, String fileName) {
+        Writer writer = new TransformerWriter(fileName);
         writer.open();
 
         LPFileGenerator lpFileGenerator = new LPFileGenerator(writer);
-        LPTransformer lpTransformer = new LPTransformer(prepareItems(), lpFileGenerator);
+        LPTransformer lpTransformer = new LPTransformer(items, lpFileGenerator);
         lpTransformer.transform();
 
         writer.close();
     }
 
-    private static List<Position> prepareItems() {
+    private static List<Position> getSimpleTriangle() {
         List<Position> items = new ArrayList<>();
 
-        /*
         items.add(new Position(0, 0));
         items.add(new Position(0.5, 0));
         items.add(new Position(0.5, 0.5));
-        */
 
+        return items;
+    }
+
+    private static List<Position> getSimpleRectangle() {
+        List<Position> items = new ArrayList<>();
 
         items.add(new Position(0, 0));
         items.add(new Position(0.5, 0));
         items.add(new Position(0, 2));
         items.add(new Position(0.5, 2));
 
+        return items;
+    }
 
-        /*for (int i = 0; i < 500; i++) {
+    private static List<Position> getRandomDistantPoints() {
+        List<Position> items = new ArrayList<>();
+
+        for (int i = 0; i < 500; i++) {
             Position pos = new Position(Math.random() * 10, Math.random() * 10, Math.random() * 10,
                     Math.random() * 10, Math.random() * 10, Math.random() * 10, Math.random() * 10,
                     Math.random() * 10, Math.random() * 10, Math.random() * 10);
             items.add(pos);
-        }*/
+        }
 
         return items;
     }
